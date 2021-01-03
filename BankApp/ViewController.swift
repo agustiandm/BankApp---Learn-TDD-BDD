bf3127233a4d5dbd7ead677981cc0ae77b36ca73
mydm <ikhwanagus01@gmail.com>
2021-01-03 17:38:31 +0700

    Initial Commit

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Agustian DM on 03/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
